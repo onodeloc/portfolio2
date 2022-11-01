765c93fc3502cbb7f95ae103dce8099d14bce57d
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-10-31 20:02:35 -0700

    worked on HTML/CSS skills

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import About from "./components/About";
+
+function App() {
+  return (
+    <main >
+      <About />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+    return (
+        <section id="default">
+            <h1>testing html, css, and react!</h1>
+        </section>
+    );
+}
