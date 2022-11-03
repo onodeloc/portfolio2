ba78a7083e54909b7601b414771dfe1855aa6eee
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-11-03 00:13:10 -0700

    Added favicon and tab title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //import logo from './logo.svg';
 import './App.css';
-import React from "react";
+import React from 'react';
+import { Helmet } from 'react-helmet'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from "./components/Nav";
 import Home from "./components/Home";
@@ -8,9 +9,14 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 
+const TITLE = 'Cole\'s website'
+
 function App() {
   return (
     <>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
       <Nav />
       <BrowserRouter>
         <Routes>
