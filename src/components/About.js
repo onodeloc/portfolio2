e8218762cda850d181f1723a369972340026d444
Cole Beezley <62979419+onodeloc@users.noreply.github.com>
2022-11-09 21:47:38 -0800

    Added a picture, messed with containers

--- src/components/About.js
+++ src/components/About.js
@@ -7,13 +7,19 @@ const About = () => (
   <div>
     <br></br>
     <h1 className="about">About me</h1>
-    <p id="two">
-      My name is Cole Beezley.<br></br>
-      Welcome to my (in-development) website.<br></br>
-      I received my Bachelor of Computer Science in May 2022.<br></br>
-      I enjoy using a variety of programming languages and tools to develop my front-end and back-end development abilities.<br></br>
-      I'm currently working on advancing my full-stack abilities.
-    </p>
+    <div id='container'>
+      
+      <p id="two">
+
+        Welcome to my (in-development) website.<br></br>
+        I received my Bachelor of Computer Science in May 2022.<br></br>
+        I enjoy using a variety of programming languages and tools to develop my front-end and back-end development abilities.<br></br>
+        I'm currently working on advancing my full-stack abilities.
+      </p>
+      
+      <img src="/images/headshot2.jpg" alt="" style={{ width: 200, height: 200 }} />
+    </div>
+
   </div>
 );
 
