361ef7ec9420da9692b277e63e850a91b828f300
Cole Beezley <cdbbeezley@gmail.com>
2022-12-10 23:49:10 -0800

    fixed grammar

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -13,7 +13,7 @@ const Projects = () => (
             Here are some of the projects in which I've used these skills. <br></br></p>
           <p id="two">
           – <a class="change" href="https://github.com/onodeloc/portfolio2">This website:</a>Built using Node.js and React<br></br>
-          – <a class="change" href="https://github.com/onodeloc/rain">Rain</a>A Next.js project to tell you if it's raining<br></br>
+          – <a class="change" href="https://github.com/onodeloc/rain">Rain:</a>A Next.js project to tell you if it's raining<br></br>
           – <a class="change" href="https://onodeloc.itch.io/knightsbane">KnightsBane:</a>A C# Unity game, available to play right now!<br></br>
           <iframe title="itch.io widget" frameborder="0" src="https://itch.io/embed/1830861" width="552" height="167"><a href="https://onodeloc.itch.io/knightsbane">KnightsBane by onodeloc</a></iframe>
           {/* – <a class="change" href="https://onodeloc.com/rain">A rain application!</a><br></br> */}
